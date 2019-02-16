3d260d62a42ae5611d97ed62975441b61b5faea1
emrecang <emre1907can1907@gmail.com>
2019-02-16 22:22:14 +0300

    Raycast

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            //Debug.DrawLine(ray.origin, hit.point, Color.red);
+            // Debug.Log(hit.point);
+            // Debug.Log(ray.origin);
+
+            //TODO player ve raycatsini önle.
+        }
+        // Debug.Log(Input.mousePosition);
+    }
+}
