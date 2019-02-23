51b9850512e877b13d0e82e62c75e4504b80fc4f
riwmd <kubilayege95@gmail.com>
2019-02-23 17:23:51 +0300

    TrapSpawning
    first update on trap spawning where the player looks.

--- Assets/Scripts/TrapPlacer.cs
+++ Assets/Scripts/TrapPlacer.cs
@@ -6,21 +6,30 @@ public class TrapPlacer : MonoBehaviour
 {
     private Camera fpsCam;
     private LineRenderer fpsLine;
-    float maxDistance;
+    float maxDistance = 5f;
+    public GameObject[] Trap;
 
     private void Start()
     {
         fpsLine = GetComponent<LineRenderer>();
-        fpsCam = GetComponent<Camera>();
+        fpsCam = GetComponentInChildren<Camera>();
     }
-    private void FixedUpdate()
+    public void FixedUpdate()
     {
         Vector3 originAim = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
-        Raycast hit;
-
-        //if(Physics.Raycast(originAim,fpsCam.transform.forward,  hit ))
-        //{
-
-        //}
+        RaycastHit hit;
+        if (Physics.Raycast(originAim, fpsCam.transform.forward, out hit, maxDistance))
+        {
+            //if(Input.GetKeyDown(KeyCode.Space))
+            //{
+                //Sadece duvar için pozisyon. Yer için farklı olacak
+                Vector3 spawnPos = new Vector3(hit.point.x, hit.transform.position.y, hit.point.z);
+                Instantiate(Trap[0], spawnPos, hit.transform.rotation);
+            //}
+            
+            GameObject candidateWall = hit.collider.gameObject;
+            WallCalculation cwScript = candidateWall.GetComponent<WallCalculation>();
+            Debug.Log(hit.transform.position);
+        }
     }
 }
